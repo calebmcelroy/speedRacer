2e95696d6c414c1a5d6833b8b0c5bc72354b9ac4
Caleb McElroy <calebmcelroy@gmail.com>
2018-08-19 19:10:15 -0400

    test script with more pins and http server

--- Sources/Server/test.js
+++ Sources/Server/test.js
@@ -1,10 +1,76 @@
 const raspi = require("raspi");
 const gpio = require("raspi-gpio");
+var url = require('url');
 
 raspi.init(() => {
   const input = new gpio.DigitalInput({ pin: "GPIO6", pullResistor: gpio.PULL_DOWN });
+  const input2 = new gpio.DigitalInput({ pin: "GPIO13", pullResistor: gpio.PULL_DOWN });
+  const input3 = new gpio.DigitalInput({ pin: "GPIO19", pullResistor: gpio.PULL_DOWN });
 
   setInterval(function() {
-    console.log(input.read());
+    console.log("Release: "+input.read());
+    console.log("Track 1: "+input2.read());
+    console.log("Track 2: "+input3.read());
   }, 10);
+  
+  
 });
+
+
+http.createServer(function(req, res) {
+
+    if (req.method === "GET") {
+
+        var call = url.parse(req.url, true);
+
+        // allow any origin to make API calls.
+        res.setHeader('Access-Control-Allow-Origin', '*');
+
+        processRequest(call.pathname, req, res);
+
+    } else {
+        res.writeHead(400);
+        res.end(JSON.stringify({
+            error: "method not implemented"
+        }));
+    }
+
+}.bind({
+    CONFIG: CONFIG
+})).listen(CONFIG.HTTP_PORT);
+
+function processRequest(method, req, res) {
+
+    switch (method) {
+        case "/get/state": // Retrieve track/application state
+            res.writeHead(200);
+            res.end();
+            //res.end(JSON.stringify(strip_gpioCtl(CONFIG, null, '\n')));
+            break;
+
+        case "/set/led": // Manually toggle LED state
+            res.writeHead(200);
+            res.end();
+            /*res.end(JSON.stringify({
+                command_sent: true
+            }));
+            CONFIG.LED_INDICATOR.ctl.toggle();*/
+            break;
+
+        case "/set/reset": // Soft reset button (in addition to physical one)
+            res.writeHead(200);
+            res.end();
+            /*res.end(JSON.stringify({
+                command_sent: true
+            }));
+            resetState();*/
+            break;
+
+        default: // Unhandled API method
+            res.writeHead(400);
+            res.end(JSON.stringify({
+                error: "method not implemented"
+            }));
+    }
+
+}
